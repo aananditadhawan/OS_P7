54ec17d3886c5e3e78b116357dda8014dcc797bd
ILAN KARCHMER <ilan@royal-20.cs.wisc.edu>
2023-12-05 16:20:21 -0600

    mount functions written

--- mount.wfs.c
+++ mount.wfs.c
@@ -11,42 +11,117 @@ struct wfs_sb superblock;
 int disk_fd;
 
 static int wfs_getattr(const char *path, struct stat *stbuf) {
-    // Implement the getattr FUSE callback
-    // This function should fill in the attributes of the file/directory specified by the path
-    // Use the superblock and disk_fd global variables
+  int res;
 
-    return 0;  // Return 0 on success
+  res = lstat(path, stbuf);
+  if (res == -1)
+    return -errno;
+
+  return 0;
 }
 
 static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
-    // Implement the readdir FUSE callback
-    // This function should fill in the directory entries using the filler function
-    // Use the superblock and disk_fd global variables
+  DIR *dp;
+  struct dirent *de;
+  (void) offset;
+  (void) fi;
 
-    return 0;  // Return 0 on success
+  dp = opendir(path);
+  if (dp == NULL)
+    return -errno;
+
+  while ((de = readdir(dp)) != NULL) {
+    struct stat st;
+    memset(&st, 0, sizeof(st));	
+    st.st_ino = de->d_ino;	
+    st.st_mode = de->d_type << 12;
+    if (filler(buf, de->d_name, &st, 0))	
+      break;
+    }
+    closedir(dp);	
+    return 0;
 }
 
-static int wfs_open(const char *path, struct fuse_file_info *fi) {
-    // Implement the open FUSE callback
-    // This function should open the file specified by the path and update the fi structure
-    // Use the superblock and disk_fd global variables
+static int wfs_write(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+  int fd;
+  int res;
 
-    return 0;  // Return 0 on success
+  (void) fi;
+  fd = open(path, O_WRONLY);
+  if (fd == -1)
+    return -errno;
+
+  res = pwrite(fd, buf, size, offset);
+  if (res == -1)
+    res = -errno;
+
+  close(fd);
+  return res;
 }
 
 static int wfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
-    // Implement the read FUSE callback
-    // This function should read data from the file specified by the path
-    // Use the superblock and disk_fd global variables
+  int fd;
+  int res;
 
-    return 0;  // Return 0 on success
+  (void) fi;
+  fd = open(path, O_RDONLY);
+  if (fd == -1)
+    return -errno;
+
+  res = pread(fd, buf, size, offset);
+  if (res == -1)
+    res = -errno;
+
+  close(fd);
+  return res;
+}
+
+static int wfs_mknod(const char* path, mode_t mode, dev_t rdev) {
+  int res;
+
+  if (S_ISREG(mode)) {
+    res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+  if (res >= 0)
+    res = close(res);
+  } else if (S_ISFIFO(mode))
+    res = mkfifo(path, mode);
+  else		
+    res = mknod(path, mode, rdev);
+  if (res == -1)
+    return -errno;
+
+  return 0;
+}
+
+static int wfs_mkdir(const char* path, mode_t mode) {
+  int res;
+
+  res = mkdir(path, mode);
+  if (res == -1)
+    return -errno;
+
+  return 0;  
+}
+
+static int wfs_unlink(const char* path) {
+  int res;
+
+  res = unlink(path);
+  if (res == -1)
+    return -errno;
+
+  return 0;
 }
 
 static struct fuse_operations wfs_operations = {
-    .getattr = wfs_getattr,
-    .readdir = wfs_readdir,
-    .open = wfs_open,
-    .read = wfs_read,
+    .getattr	= wfs_getattr,
+    .mknod      = wfs_mknod,
+    .mkdir      = wfs_mkdir,
+    .read	= wfs_read,
+    .write      = wfs_write,
+    .readdir	= wfs_readdir,
+    .unlink    	= wfs_unlink,
+
 };
 
 int main(int argc, char *argv[]) {
